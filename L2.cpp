e41ecd10660099dfc56b03024ef3bde67d9a7f0b
Sawon <141425167+SawonAnis@users.noreply.github.com>
2024-01-22 00:34:39 +0600

    Add files via upload

---
+++ L2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int i,sum=0;
+
+for (i=1; i<=10; i++)
+{
+    cout<<i<<" ";
+    sum=sum+i;
+}
+  cout<<"\nSum of the First 10 Number: "<<sum<<endl;
+
+return 0; 
+} 
+
+
+
+
