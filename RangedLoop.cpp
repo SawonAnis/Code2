3e53fde1670ed3646cbbb201f2ee5ceb1a8acfa9
Sawon <141425167+SawonAnis@users.noreply.github.com>
2024-02-22 07:56:58 +0600

    Add files via upload
    Basic RangedLoop.

---
+++ RangedLoop.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int ArrayR[] = {10, 20, 30, 40, 50, 60, 70, 80};
+
+    for(int i:ArrayR){
+        cout << i << " ";
+    }
+
+    return 0;
+}
