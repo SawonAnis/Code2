0e36e8bbaa23a3b6d17d655a8dca2a2026342a11
Sawon <141425167+SawonAnis@users.noreply.github.com>
2024-05-27 01:38:00 +0600

    ASCII.cpp

--- ASCII.cpp
+++ ASCII.cpp
@@ -4,13 +4,11 @@ using namespace std;
 
 int main()
 {
-    ///Capital letter
     char ch;
     for(ch='A';ch<='Z';ch++)
     {
         cout<<"ASCII Value of : "<<ch<<"="<<(int)ch<<endl;
     }
-    ///Small letter
     for(ch='a';ch<='z';ch++)
     {
         cout<<"ASCII Value of : "<<ch<<"="<<(int)ch<<endl;
